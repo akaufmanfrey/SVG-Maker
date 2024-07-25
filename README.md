94c36e1ba685c27bee91e541f4e900f8a82e3e38
akaufmanfrey <akaufmanfrey@gmail.com>
2024-07-25 12:50:09 -0400

    commented code and updated README

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# SVG-Maker
-Generates SVG images based on text inputs
+# SVG-Maker 
+## Table of Contents
+1. [Description](#description)
+2. [Installation](#installation)
+3. [Usage](#usage)
+4. [License](#license)
+5. [Contributing](#contributing)
+6. [Tests](#tests)
+7. [Questions](#questions) 
+## Description
+A tool to generate a rudimentary logo as an SVG file. When index is ran, the program prompts the user for various inputs which are then used to generate the image.
+Video of the program: https://app.screencastify.com/v3/watch/xmZTdUBrtNztEIrgmjsR
+## Installation
+Install the project into any directory. The logo file is generated in the examples folder.
+## Usage
+Run the program using node, answer the resulting prompts, and the image will be generated for you.
+## License
+[![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/MIT)
+
+Click the above badge to see the license.
+## Contributing
+N/A
+## Tests
+A testing file is in the project called 'shapes.test.js'. This file can be run using jest which, if installed, can be executed using 'node test'.
+## Questions
+Github: https://github.com/akaufmanfrey 
--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const fs = require('fs');
 const {Shape, Triangle, Square, Circle} = require('./lib/shapes.js');
 const inquirer = require('inquirer');
 
-inquirer
+// prompt for getting user inputs for text, text color, shape, and shape color
+function init() {
+    inquirer
   .prompt([
     {
       type: "input",
@@ -27,17 +29,27 @@ inquirer
     },
   ])
   .then((answers) => {
-    var logo;
-    switch(answers.shape) {
-        case 'square':
-            logo = new Square(answers.shapeColor, answers.text, answers.textColor);
-            break;
-        case 'triangle':
-            logo = new Triangle(answers.shapeColor, answers.text, answers.textColor);
-            break;
-        case 'circle':
-            logo = new Circle(answers.shapeColor, answers.text, answers.textColor);
-            break; 
+    // if more than 3 characters entered, log the issue and restart the prompt
+    if (answers.text.length > 3) {
+        console.log('Please enter only up to 3 characters for text');
+        init();
+    } else {
+        var logo;
+        // checks to see which option was chosen for shape and instantiates a shape
+        switch(answers.shape) {
+            case 'square':
+                logo = new Square(answers.shapeColor, answers.text, answers.textColor);
+                break;
+            case 'triangle':
+                logo = new Triangle(answers.shapeColor, answers.text, answers.textColor);
+                break;
+            case 'circle':
+                logo = new Circle(answers.shapeColor, answers.text, answers.textColor);
+                break; 
     }
-    fs.writeFile('./examples/logo.svg', logo.render(), (err) => err? console.error(err) : console.log('Generated logo.svg'));
-  });
+        // writes the svg string to the logo file
+        fs.writeFile('./examples/logo.svg', logo.render(), (err) => err? console.error(err) : console.log('Generated logo.svg'));
+  }});
+}
+// call to start the program
+init();
